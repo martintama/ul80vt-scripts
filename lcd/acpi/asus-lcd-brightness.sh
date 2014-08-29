e133e304edd9ebb8d3ab384d4bada7a37052f66d
Martin Tamashiro <martintama@gmail.com>
2014-08-29 01:42:05 -0300

    Saco echoes innecesarios en script lcd

--- lcd/acpi/asus-lcd-brightness.sh
+++ lcd/acpi/asus-lcd-brightness.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-echo "si!"
+#echo "si!"
 # this directory is a symlink on my machine:
 KEYS_DIR=/sys/class/backlight/intel_backlight 
 
-echo "1"
+#echo "1"
 test -d $KEYS_DIR || exit 0
-echo "2"
+#echo "2"
 MIN=50000
 MAX=$(cat $KEYS_DIR/max_brightness)
 VAL=$(cat $KEYS_DIR/brightness)
 
-echo "Actual: " $VAL
+#echo "Actual: " $VAL
 if [ "$1" = down ]; then
 	VAL=$((VAL-50000))
 else
 	VAL=$((VAL+50000))
 fi
-echo "Nuevo: " $VAL
+#echo "Nuevo: " $VAL
 
 if [ "$VAL" -lt $MIN ]; then
 	VAL=$MIN
